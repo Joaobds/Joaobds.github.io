ec839edd639c593be1e1eaadb08e54cc3c82aae9
joao.b <joao.b@sc.senai.br>
2024-09-05 12:11:30 -0300

    fix: Ajustes para resolver cross-origin

--- ckeditor/plugins/FMathEditor/dialogs/dialog.js
+++ ckeditor/plugins/FMathEditor/dialogs/dialog.js
@@ -1,71 +1,74 @@
-﻿var dialog = CKEDITOR.dialog.add( 'FMathEditorDialog', function( editor ) {
-    var editorIFrameID = "editorIFrame" + editor.name;
- 	return {
-        title: 'FMath Editor - www.fmath.info',
-        minWidth: 1020,
-        minHeight: 490,
-        contents: [
-            {
-                id: 'tab-basic',
-                label: 'Basic Settings',
-                elements: [
-                    {
-                        type: 'html',
-                        html: '<iframe id="' +editorIFrameID +'" style="width:1024px;height:500px" src="'+ CKEDITOR.basePath +'plugins/FMathEditor/editor/onlyEditor.html"></iframe>'
-                    }
-                ]
-            }
-        ],
-        onOk: function() {
-            var dialog = this;
-			var iframe = document.getElementById(editorIFrameID);
-			iframe.contentWindow.postMessage("getMathML", "https://joaobds.github.io");
-			window.addEventListener("message", function(event) {
-				let mathml = event.data;
+﻿var dialog = CKEDITOR.dialog.add('FMathEditorDialog', function(editor) {
+	var editorIFrameID = "editorIFrame" + editor.name;
+	var iframe = null;
+	var mathml = null;
 
-				document.getElementById(editorIFrameID).contentWindow.getBlobOrUrl(function(result) {
-					if (result.indexOf("ERROR:") == 0) {
-						alert(result);
-					} else {
-						var img = result;
-						var selection = editor.getSelection();
-						if (selection.getType() == CKEDITOR.SELECTION_ELEMENT) {
-							var selElem = selection.getSelectedElement();
-							if (selElem.getName() == 'img') {
-								selElem.data('cke-saved-src', img);
-								selElem.setAttribute("src", img);
-								selElem.setAttribute("alt", "MathML (base64):" + window.btoa(mathml));
-								return;
-							}
-						}
-						var imgElem = editor.document.createElement('img');
-						imgElem.setAttribute("src", img);
-						imgElem.setAttribute("alt", "MathML (base64):" + window.btoa(mathml));
-						editor.insertElement(imgElem);
+	return {
+		title: 'FMath Editor - www.fmath.info',
+		minWidth: 1020,
+		minHeight: 490,
+		contents: [
+			{
+				id: 'tab-basic',
+				label: 'Basic Settings',
+				elements: [
+					{
+						type: 'html',
+						html: '<iframe id="' + editorIFrameID + '" style="width:1024px;height:500px" src="' + CKEDITOR.basePath + 'plugins/FMathEditor/editor/onlyEditor.html"></iframe>'
 					}
-				});
-			}, { once: true });
+				]
+			}
+		],
+		onOk: function() {
+			var dialog = this;
 
-        },
-        onShow: function(){
+			iframe.contentWindow.postMessage({ type: 'getMathML' }, '*');
+
+			window.addEventListener('message', function(event) {
+
+				if (event.data.type === 'mathMLResponse') {
+					mathml = event.data.mathml;
+
+					document.getElementById(editorIFrameID).contentWindow.getBlobOrUrl(function(result) {
+						if (result.indexOf("ERROR:") == 0) {
+							alert(result);
+						} else {
+							var img = result;
+							var selection = editor.getSelection();
+							if (selection.getType() == CKEDITOR.SELECTION_ELEMENT) {
+								var selElem = selection.getSelectedElement();
+								if (selElem.getName() == 'img') {
+									selElem.data('cke-saved-src', img);
+									selElem.setAttribute("src", img)
+									selElem.setAttribute("alt", "MathML (base64):" + window.btoa(mathml));
+									return;
+								}
+							}
+							var imgElem = editor.document.createElement('img');
+							imgElem.setAttribute("src", img)
+							imgElem.setAttribute("alt", "MathML (base64):" + window.btoa(mathml));
+							editor.insertElement(imgElem);
+						}
+					});
+				}
+			}, false);
+		},
+		onShow: function() {
+			iframe = document.getElementById(editorIFrameID);
 			var selection = editor.getSelection();
 			if (selection.getType() == CKEDITOR.SELECTION_ELEMENT) {
-				//var selectedContent = selection.getSelectedElement().$.outerHTML;
 				var selElem = selection.getSelectedElement();
-				if(selElem.getName() =='img'){
+				if (selElem.getName() == 'img') {
 					var mathmlEnc = selElem.getAttribute("alt");
-					if(mathmlEnc!=null && mathmlEnc.length > 16){
+					if (mathmlEnc != null && mathmlEnc.length > 16) {
 						mathmlEnc = mathmlEnc.substring(16, mathmlEnc.length);
-
 						var mathml = window.atob(mathmlEnc);
-						if(mathml.indexOf("<math")==0){
-							document.getElementById(editorIFrameID).contentWindow.setMathML(mathml);
+						if (mathml.indexOf("<math") == 0) {
+							iframe.contentWindow.postMessage({ type: 'setMathML', mathml: mathml }, '*');
 						}
 					}
 				}
-
 			}
 		},
-    };
+	};
 });
-
